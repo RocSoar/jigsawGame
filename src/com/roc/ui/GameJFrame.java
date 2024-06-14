8217bea51d26cbdc35e7c495d938fb6942704c07
RocSoar <lsyyd3454@outlook.com>
2024-06-14 22:09:45 +0800

    add assets

--- src/com/roc/ui/GameJFrame.java
+++ src/com/roc/ui/GameJFrame.java
@@ -1,18 +1,49 @@
 package com.roc.ui;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.WindowConstants;
 
+import com.roc.utils.ArrayUtils;
+
 public class GameJFrame extends JFrame {
+    int[][] data = new int[4][4];
+
     public GameJFrame() {
         initJFrame();
 
         initJMenuBar();
 
+        initData();
+
+        initImage();
+
         setVisible(true);
+    }
+
+    private void initData() {
+        int[] tempArr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        ArrayUtils.disorderArrayInPlace(tempArr);
+        for (int i = 0; i < tempArr.length; i++) {
+            data[i / 4][i % 4] = tempArr[i];
+        }
+    }
+
+    private void initImage() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                ImageIcon icon = new ImageIcon(
+                        "D:/Learning/Java/jigsawGame/image/animal/animal3/" + data[i][j] + ".jpg");
+                JLabel jLabel = new JLabel(icon);
+                jLabel.setBounds(105 * j, 105 * i, 105, 105);
+                getContentPane().add(jLabel);
+            }
+        }
+
     }
 
     private void initJMenuBar() {
@@ -47,5 +78,7 @@ public class GameJFrame extends JFrame {
         setLocationRelativeTo(null);
         // 设置关闭模式
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLayout(null);
     }
 }
--- src/com/roc/utils/ArrayUtils.java
+++ src/com/roc/utils/ArrayUtils.java
@@ -3,11 +3,11 @@ package com.roc.utils;
 import java.util.Random;
 
 public class ArrayUtils {
-    public static <T> void disorderArrayInPlace(T[] arr) {
+    public static void disorderArrayInPlace(int[] arr) {
         Random r = new Random();
         for (int i = 0; i < arr.length; i++) {
             int randomIndex = r.nextInt(arr.length);
-            T temp = arr[i];
+            int temp = arr[i];
             arr[i] = arr[randomIndex];
             arr[randomIndex] = temp;
         }
