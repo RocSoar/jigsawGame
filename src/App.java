f814f8811670e356398dbedfe535cff3b01b675f
rocsoar_mac <lsyyd3454@outlook.com>
2024-07-12 00:43:22 +0800

    add properties config.

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 
 public class App {
     public static void main(String[] args) throws IOException {
-//        new LoginJFrame();
-        new GameJFrame("roc");
+        new LoginJFrame();
+//        new GameJFrame("roc");
     }
 }
